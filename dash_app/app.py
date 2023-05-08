7604375c435d77fc5b9215b4401d7c4c74231051
baldogiovine <129193548+baldogiovine@users.noreply.github.com>
2023-05-08 14:01:34 +0200

    Delete dash_app directory

--- dash_app/app.py
+++
@@ -1,5 +0,0 @@
-import dash
-import dash_bootstrap_components as dbc
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME], suppress_callback_exceptions=True)
-server = app.server
