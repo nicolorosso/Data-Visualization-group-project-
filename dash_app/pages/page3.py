d6822b25610f9f41fac7e23d82772e7253aedf71
Nicolò Rosso <98038058+nicolorosso@users.noreply.github.com>
2023-05-16 02:43:55 +0200

    Update page3.py

--- dash_app/pages/page3.py
+++ dash_app/pages/page3.py
@@ -1,15 +1,16 @@
-import dash_html_components as html
-import dash_core_components as dcc
-from dash.dependencies import Input, Output, State
-from app import app  # Import app from app.py
+import dash
+from dash import dcc, html, Input, Output, State, callback
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 import pandas as pd
 import json
-import networkx as nx
-import plotly.express as px
+import numpy as np
+import dash_bootstrap_components as dbc
+
+dash.register_page(__name__, name='Page 2')
 
 
-with open("edge_betweenness_dataframes.json", 'r') as infile:
+with open("data/edge_betweenness_dataframes.json", 'r') as infile:
     loaded_json_data = json.load(infile)
 
 # Convert the JSON objects to dataframes
@@ -18,100 +19,162 @@ result_dfs_edge_betweenness = {}
 for kw, json_obj in loaded_json_data.items():
     result_dfs_edge_betweenness[kw] = pd.read_json(json_obj, orient='records')
 
-# Define the app layout
-import dash_core_components as dcc
+image_path = 'assets/twitter_network.png'
 
-layout = html.Div([
-    html.Br(),
-    html.Br(),
-    html.Br(),
-    html.H1('Top 10 Community Network Graphs', style={'textAlign': 'center'}),
-    html.Div([
-        html.P(
-            """
-            This interactive network graph shows the relationships between different communities discussing the selected keyword.
-            The graph is based on the edge betweenness of the communities. Use the dropdown menu to select a keyword and explore the network graph.
-            """, className='lead', style={"marginTop": "40px", "fontSize": "20px", "textAlign": "justify", "textJustify": "inter-word"}
-            #style={"textAlign": "justify", "margin": "20px"},
-        ),
-    ]),
-    dcc.Graph(id='network-graph'),
-    dcc.Dropdown(
-        id='keyword-dropdown',
-        options=[{"label": kw, "value": i} for i, kw in enumerate(result_dfs_edge_betweenness.keys())],
-        value=0,
-        clearable=False,
-        style={"width": "50%", "margin": "0 auto"},
-    ),
-    html.Br(),
-    dcc.Link('Go back to the main page', href='/'),
-    html.Div([dcc.Input(id='dummy-input', type='hidden', value='dummy')]),
+faq_section = html.Div([
+    dbc.Button("FAQ", id="faq_toggle", className="mb-3"),
+    dbc.Collapse([
+        dbc.Card([
+            dbc.CardHeader("1. Identify the most common keywords related to the political discourse."),
+            dbc.CardBody(
+                "In this analysis, we first identify the top 7 keywords that are most frequently mentioned in the collected Twitter data. This helps to narrow down the focus of our analysis on the most relevant topics in the political discourse."),
+        ]),
+        dbc.Card([
+            dbc.CardHeader("2. Use the greedy modularity algorithm to find communities for each keyword."),
+            dbc.CardBody(
+                "We then use the greedy modularity algorithm to identify communities for each keyword. This algorithm is a popular method for detecting communities in networks, allowing us to group users that are closely related in the context of the keyword."),
+        ]),
+        dbc.Card([
+            dbc.CardHeader("3. Compute the level of agreement within each community using the RoBERTa-large algorithm."),
+            dbc.CardBody(
+                "Next, we compute the level of agreement within each community using the RoBERTa-large algorithm, which is a state-of-the-art natural language processing model. By analyzing the sentiment and content of the tweets, this algorithm allows us to understand the degree of agreement among users within each community."),
+        ]),
+        dbc.Card([
+            dbc.CardHeader("4. Visualize the level of polarization within communities using the Kruskal-Wallis test."),
+            dbc.CardBody(
+                "Finally, we visualize the level of polarization within communities using the Kruskal-Wallis test. This statistical test helps us compare the distributions of edge betweenness within communities, allowing us to observe how polarized the communities are based on the selected keywords."),
+        ]),
+    ], id="faq_collapse"),
 ])
 
 
-def create_network_graph(keyword, df):
-    # Group the data by SourceModularity and TargetModularity
-    grouped = df.groupby(['SourceModularity', 'TargetModularity'])
 
-    # Filter out self-looping communities
-    filtered_groups = [(name, group) for name, group in grouped if name[0] != name[1]]
+layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1('Unveiling Polarization: A Twitter Discourse Analysis',
+                    style={'textAlign': 'center', 'font-weight': 'bold', 'color': 'black'}),
+            html.P("""Journey with us as we delve into the Twitterverse, harnessing the power of keywords to unravel political discourse.
+            Our exploration reveals not only the most tweeted keywords, but also the polarization within communities. This unique insight allows us 
+            to gauge the state of political discourse with unparalleled depth.
+            """,
+                    style={'textAlign': 'center', 'color': 'black','font-size': '20px'})
 
-    # Create a NetworkX graph from the filtered DataFrame
-    G = nx.from_pandas_edgelist(pd.concat([group for _, group in filtered_groups]), 'SourceModularity',
-                                'TargetModularity', edge_attr=True, create_using=nx.DiGraph())
+        ], width=12)
+    ], style={'marginBottom': '50px', 'marginTop': '50px'}),
 
-    # Get node positions using a layout algorithm (you can use other layout algorithms as well)
-    pos = nx.spring_layout(G, seed=42)
+    dbc.Row([
+        dbc.Col([
+            html.H4("Top 7 Keywords by Occurrence: The Power Players of Political Discourse",
+                    style={'textAlign': 'center', 'font-weight': 'bold', 'color': 'black'}),
+            dcc.Graph(id="keywords_barplot"),
+        ], width=6),
+        dbc.Col([
+            html.H4("Edge Betweenness Percentiles: Unveiling the Bridge Keywords",
+                    style={'textAlign': 'center', 'font-weight': 'bold', 'color': 'black'}),
+            dcc.Graph(id="edge_bet_percentiles_plot"),
+        ], width=6),
+    ], style={'marginBottom': '50px'}),
 
-    # Get a list of colors
-    colors = px.colors.qualitative.Plotly
+    dbc.Row([
+        dbc.Col([
+            html.H4("How We Did It: A Peek Behind The Curtain",
+                    style={'textAlign': 'left', 'font-weight': 'bold', 'color': 'black'}),
+            faq_section,
+        ], width=12)
+    ], style={'marginBottom': '30px', 'marginTop': '30px'}),
 
-    # Create node trace
-    node_trace = go.Scatter(
-        x=[pos[node][0] for node in G.nodes],
-        y=[pos[node][1] for node in G.nodes],
-        text=[str(node) for node in G.nodes],
-        mode='markers+text',
-        hoverinfo='text',
-        marker=dict(
-            color=[colors[int(node) % len(colors)] for node in G.nodes],
-            size=15,
-            line_width=2
+    dbc.Row([
+        dbc.Col([
+            dcc.Link('Want more? Return to the main page', href='/')
+        ], width=12, style={'textAlign': 'center', 'marginBottom': '30px', 'marginTop': '30px'}),
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dcc.Input(id='dummy-input', type='hidden', value='dummy')
+        ], width=12)
+    ])
+])
+
+@callback(
+    [Output(component_id='keywords_barplot', component_property='figure'),
+     Output(component_id='edge_bet_percentiles_plot', component_property='figure')],
+    [Input(component_id='dummy-input', component_property='value')]
+)
+
+def update_output(dummy_value):
+    # Top 5 Keywords Bar Plot
+    keywords_barplot = create_top_7_keywords_barplot()
+
+    # Edge Betweenness Percentiles Plot
+    edge_bet_percentiles_plot = create_edge_bet_percentiles_plot()
+
+    return keywords_barplot, edge_bet_percentiles_plot
+
+
+def create_top_7_keywords_barplot():
+    # Get the top 7 keywords by the number of occurrences in the result_dfs_edge_betweenness
+    top_keywords = sorted(result_dfs_edge_betweenness.keys(), key=lambda x: len(result_dfs_edge_betweenness[x]), reverse=True)[:7]
+    top_occurrences = [len(result_dfs_edge_betweenness[keyword]) for keyword in top_keywords]
+
+    # Create the bar plot
+    fig = go.Figure(go.Bar(x=top_keywords, y=top_occurrences))
+    fig.update_layout(
+                      xaxis_title='Keywords',
+                      yaxis_title='Occurrences')
+    return fig
+
+
+
+def create_edge_bet_percentiles_plot():
+    percs = np.linspace(80, 100, 500)
+    fig = go.Figure()
+
+    for idx, (keyword, df) in enumerate(result_dfs_edge_betweenness.items()):
+        edges_agreement = df.loc[df.agreement == 1].edge_bet.values
+        edges_disagreement = df.loc[df.agreement == -1].edge_bet.values
+
+        qn_edges_agreement = np.percentile(edges_agreement, percs)
+        qn_edges_disagreement = np.percentile(edges_disagreement, percs)
+
+        fig.add_trace(
+            go.Scatter(x=percs[450:], y=qn_edges_agreement[450:], name=f"{keyword} Agreement",
+                       visible=True if idx == 0 else "legendonly")
         )
-    )
 
-    # Create edge trace
-    edge_trace = go.Scatter(
-        x=[pos[edge[0]][0] for edge in G.edges] + [pos[edge[1]][0] for edge in G.edges],
-        y=[pos[edge[0]][1] for edge in G.edges] + [pos[edge[1]][1] for edge in G.edges],
-        line=dict(width=0.5, color='#888'),
-        hoverinfo='none',
-        mode='lines'
-    )
+        fig.add_trace(
+            go.Scatter(x=percs[450:], y=qn_edges_disagreement[450:], name=f"{keyword} Disagreement",
+                       visible=True if idx == 0 else "legendonly")
+        )
 
-    # Create a Plotly figure
-    fig = go.Figure(data=[edge_trace, node_trace],
-                    layout=go.Layout(
-                        title=f'Network Graph for Keyword "{keyword}"',
-                        showlegend=False,
-                        hovermode='closest',
-                        margin=dict(b=20, l=5, r=5, t=40),
-                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
-                    ))
+    dropdown_buttons = [
+        dict(
+            args=[{"visible": [True if trace_idx in [2 * idx, 2 * idx + 1] else False
+                               for trace_idx in range(2 * len(result_dfs_edge_betweenness))]}],
+            label=keyword,
+            method="update"
+        ) for idx, keyword in enumerate(result_dfs_edge_betweenness)
+    ]
+
+    fig.update_layout(
+        updatemenus=[go.layout.Updatemenu(buttons=dropdown_buttons, showactive=True)],
+
+        xaxis_title="Percentiles",
+        yaxis_title="Edge Betweenness",
+        yaxis_type="log",
+        legend_title="Keywords",
+        hovermode="x"
+    )
 
     return fig
 
-# Define the callback for updating the network graph
-@app.callback(
-    Output('network-graph', 'figure'),
-    Input('keyword-dropdown', 'value')
+@callback(
+    Output("faq_collapse", "is_open"),
+    [Input("faq_toggle", "n_clicks")],
+    [State("faq_collapse", "is_open")],
 )
-def update_graph(selected_keyword_index):
-    # Get the selected keyword and DataFrame
-    keyword, df = list(result_dfs_edge_betweenness.items())[selected_keyword_index]
-
-    # Create the network graph for the selected keyword
-    figure = create_network_graph(keyword, df)
-
-    return figure
+def toggle_faq(n, is_open):
+    if n:
+        return not is_open
+    return is_open
