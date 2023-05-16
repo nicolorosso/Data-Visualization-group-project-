97d888cf861e4754985852fe98ca3823b4a91c91
Nicolò Rosso <98038058+nicolorosso@users.noreply.github.com>
2023-05-16 02:45:48 +0200

    Update navbar.py

--- dash_app/pages/navbar.py
+++ dash_app/pages/navbar.py
@@ -1,6 +1,6 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-from app import app
+import dash
 
 HEADER_STYLE = {
     "width": "100%",
@@ -20,18 +20,23 @@ CONTENT_STYLE = {
 
 navbar_menu = dbc.Nav(
     [
-        dbc.NavLink("Home", href="/", active="exact", style={"color": "white", "font-weight": "bold"}),
-        dbc.NavLink("Page 1", href="/page-1", active="exact", style={"color": "white", "font-weight": "bold"}),
-        dbc.NavLink("Page 2", href="/page-2", active="exact", style={"color": "white", "font-weight": "bold"}),
-        dbc.NavLink("Page 3", href="/page-3", active="exact", style={"color": "white", "font-weight": "bold"}),
-    ],
-    style={"display": "flex", "justify-content": "center", "align-items": "center"},
+        dbc.NavLink(
+            [
+                html.Div(page["name"], className="ms-2"),
+                ],
+                href=page["path"],
+                active="exact",
+                style={"color": "white", "font-weight": "bold"}
+                )
+                for page in dash.page_registry.values()
+            ],
+            style={"display": "flex", "justify-content": "center", "align-items": "center"},
 )
 
 header_menu = html.Div(
     [
         html.Div([
-            html.Img(src='assets/twitter_logo.png', alt="Twitter Logo", #app.get_asset_url('assets/twitter_logo.png')
+            html.Img(src='assets/twitter_logo.png', alt="Twitter Logo",
                      style={"width": "2rem", "height": "2rem"}),
         ], style={"flex": "1"}),
         html.Div([
